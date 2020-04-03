63549aaa985457f348e3d536093b91e2922145fe
Ayan Pahwa <iayanpahwa@gmail.com>
2020-04-03 11:17:15 +0530

    Added code for Neopixel strip on badge lanyard

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import time
 import board
 import busio
+import neopixel
 from digitalio import DigitalInOut
 from adafruit_minimqtt import MQTT
 from adafruit_pybadger import PyBadger
@@ -20,10 +21,20 @@ esp32_cs = DigitalInOut(board.D13)
 esp32_ready = DigitalInOut(board.D11)
 esp32_reset = DigitalInOut(board.D12)
 
+# Lanyard Neopixel Setup
+pixel_pin = board.D2
+num_pixels = 50
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+                           pixel_order=ORDER)
+pixels.fill((0, 0, 0))
+pixels.show()
+
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
 pybadger = PyBadger()
+#pybadger.show_badge(name_string="AYAN", hello_scale=2, my_name_is_scale=2, name_scale=3)
 pybadger.pixels.fill(0)
 
 # Setup a feed hierarchy for subscribing to changes
@@ -34,33 +45,37 @@ def connected(client, userdata, flags, rc):
     # successfully to the broker.
     print('Connected to MQTT Broker! Listening for topic changes on %s' % subscribe_feed)
     client.subscribe(subscribe_feed)
-    # basic badge render on successful connect of broker 
-    pybadger.show_badge(name_string="<Your_Name>", hello_scale=2, my_name_is_scale=2, name_scale=3)
+    # basic badge render
+    pybadger.show_badge(name_string="Ayan", hello_scale=2, my_name_is_scale=2, name_scale=3)
 
 def disconnected(client, userdata, rc):
     # This method is called when the client is disconnected
     print('Disconnected from MQTT BROKER!')
 
+
 def message(client, topic, message):
     print('New message on topic {0}: {1}'.format(topic, message))
 
     # feed #1 on which we're listening to values of color is named RGB
-    if topic == secrets['aio_username'] + '/feeds/RGB':
+    if topic == "iayanpahwa/feeds/RGB":
         RGB = [x.strip() for x in message.split('#')]
         val = int(RGB[1], 16)
         pybadger.pixels.fill(val)
+        pixels.fill(val)
+        pixels.show()
         time.sleep(0.1)
 
     # feed #1 on which we're listening to values of name is named name
-    elif topic == secrets['aio_username'] + '/feeds/name':
+    elif topic == "iayanpahwa/feeds/name":
         pybadger.show_badge(name_string=message, hello_scale=2, my_name_is_scale=2, name_scale=3)
+
 
 wifi.connect()
 
-# Set up a MiniMQTT Client with broker address and api keys
+# Set up a MiniMQTT Client
 mqtt_client = MQTT(socket,
-                   broker='io.adafruit.com', 
-                   username=secrets['aio_username'], 
+                   broker='io.adafruit.com',
+                   username=secrets['aio_username'],
                    password=secrets['aio_key'],
                    network_manager=wifi)
 
@@ -75,3 +90,12 @@ mqtt_client.connect()
 while True:
     # Poll the message queue forever (BLOCKING!!)
     mqtt_client.loop_forever()
+
+    #pybadger.auto_dim_display(delay=10) # Remove or comment out this line if you have the PyBadge LC
+    if pybadger.button.a:
+        pybadger.show_business_card(image_name="Blinka.bmp", name_string="Ayan Pahwa", name_scale=2,
+                                    email_string_one="iayanpahwa", email_string_two="@gmail.com")
+    elif pybadger.button.b:
+        pybadger.show_qr_code(data="https://codeNsolder.com")
+    elif pybadger.button.start:
+        pybadger.show_badge(name_string="AYAN", hello_scale=2, my_name_is_scale=2, name_scale=3)
